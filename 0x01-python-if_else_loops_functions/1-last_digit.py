b4f086d468bff951ea8a21d831dcd89a13f71c12
bolajiemmanuel01 <bolajiemmanuel01@gmail.com>
2022-05-31 16:51:39 +0100

    Greater than 5

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,9 +7,9 @@ else:
     remeinder = number % (10)
 if remeinder > 5:
     print(f"Last digit of {number:d} is {remeinder:d}"
-    " and is greater than 5.")
+        " and is greater than 5")
 elif remeinder == 0:
     print(f"Last digit of {number:d} is {remeinder:d} and is 0")
 else:
     print(f"Last digit of {number:d} is {remeinder:d}"
-    " and is less than 6 and not 0")
+        " and is less than 6 and not 0")
