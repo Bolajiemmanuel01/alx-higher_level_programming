7d39fe146e8e0f18bbcd1e010435aa29de9cf700
bolajiemmanuel01 <bolajiemmanuel01@gmail.com>
2022-06-02 20:37:33 +0100

    Put it in string formt text

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     from sys import argv
     user_input= argv[1:]
     size = len(user_input)
-    print("{:d} {:s}{:s}".
+    print("{:d} {:s} {:s}".
           format(size,
                  "arguments" if (size) is not 1 else "argument",
                  "." if (size) is 0 else ":"))
-    for idx, arg in enumerate(user_input):
-        print("{:d}: {:s}".format(idx + 1, arg))
+    for n, arg in enumerate(user_input):
+        print("{:d} : {:s}".format(n + 1, arg))
