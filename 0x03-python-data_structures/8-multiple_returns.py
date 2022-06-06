c72b1d3de1d5796e5a4e2781a5616dc50c6f0f7a
bolajiemmanuel01 <bolajiemmanuel01@gmail.com>
2022-06-06 21:36:39 +0100

    Multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     my_tuple = ()
-    a = len(sentence)
-    b = sentence[0]
-    if a == 0:
+    if len(sentence) == 0:
         my_tuple = 0, "None"
         return my_tuple
     else:
-        my_tuple = a, b
+        my_tuple = len(sentence), sentence[0]
         return my_tuple
