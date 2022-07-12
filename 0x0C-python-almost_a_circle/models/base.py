a76fc67c410989f3f8d424dc81be0fea68766f35
bolajiemmanuel01 <bolajiemmanuel01@gmail.com>
2022-07-12 18:39:59 +0100

    extra text

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,9 +5,16 @@ Module contains class Base
 Contains private class __nb_objects, and class constructor __init__
 """
 class Base:
+    """
+    defines class Base
+    Class Attributes:
+        __nb_objects
+    Methods:
+        __init__(self, id=None)
+    """
     __nb_objects = 0
     def __init__(self, id=None):
-        if id != None:
+        if id:
             self.id = id
         else:
             Base.__nb_objects += 1
