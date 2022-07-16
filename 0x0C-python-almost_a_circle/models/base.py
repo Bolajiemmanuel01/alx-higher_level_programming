db2b74a5a88805ddd8a7096ddc48442ac995cffa
bolajiemmanuel01 <bolajiemmanuel01@gmail.com>
2022-07-16 17:27:32 +0100

    let's see

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,8 +33,14 @@ class Base():
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize id, increment class attribute if no id and set as id"""
-        if id:
+        """Initialization of a Base instance.
+        Args:
+            - id: id of the instance
+        """
+
+        if type(id) != int and id is not None:
+            raise TypeError("id must be an integer")
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
